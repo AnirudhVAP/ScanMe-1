c487c7586964ae5fd3dc12a9fef07df462c00bda
rajashekar <mudholker.rajashaker@gmail.com>
2024-06-26 12:38:53 +0530

    #

--- ScanMe/src/App.jsx
+++ ScanMe/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as  Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AddedItemsProvider } from './components/context/AddedItemsContext';
 import ItemDetails from './components/ItemDetails/ItemDetails';
 import LandingPage from './pages/LandingPage/Landingpage'
